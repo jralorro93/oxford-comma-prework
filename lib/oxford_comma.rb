3556c86f179ab193cba3f3904bcba1c57e2fb7aa
Juan Alorro <jralorro93@gmail.com>
2019-01-31 19:25:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     return array.join(" and ")
   elsif array.length >= 3
-    new_array = "and #{array-1}"
+    array[-1] = "and #{array-1}"
     array.pop
     array.push(new)
     return array.join(", ")
