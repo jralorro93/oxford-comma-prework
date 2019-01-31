9a5a49e3393b022f08d6739a9070c2dcab79fc47
Juan Alorro <jralorro93@gmail.com>
2019-01-31 19:23:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,7 @@ def oxford_comma(array)
     return result
   elsif array.length == 2 
     return array.join(" and ")
-  elsif array.length == 3
-    array.insert(2, 'and')
-    return array.to_s
-  else 
+  elsif array.length >= 3
     new = "and #{array-1}"
     array.pop
     array.push(new)
