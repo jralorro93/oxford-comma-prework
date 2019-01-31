62f923356a3aa887eb8ed9f6e204d8724d7dbb8c
Juan Alorro <jralorro93@gmail.com>
2019-01-31 18:56:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     result = array.split "and"
     return result
   elsif array.length == 3
-    array.insert (2, )
+    array.insert (2, "and")
     return array
   end
 end
