8565043368137906bf6af788c7ea5f74366f339e
Juan Alorro <jralorro93@gmail.com>
2019-01-31 18:27:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
   if array.length == 1 
     result = array.join
-    return array.join
+    return result
   elsif array.length == 2 
-    return array.split and
-  else 
+    result = array.split "and"
+    return result
+  els
     return array
   end
 end
