5f1dc2aa950de0ba57348b38495adf06a72548c6
Juan Alorro <jralorro93@gmail.com>
2019-01-31 18:20:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1 
     return array
   elsif array.length == 2 
-    return array.split("and")
+    return array.split and
   elsif array.length == 3 
     return array
   end
